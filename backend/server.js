73f42e201c3cec3f181ec80c3ab427435d9bca0d
Peterxbh <peterxbh@uchicago.edu>
2024-03-22 00:51:35 -0500

    Setup the Front end and Connected to Back End

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 import express from 'express';
-
-const port = 5000;
+import dotevn from 'dotenv';
+import products from './data/products.js';
+const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send("Api is running...");
+});
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
